2901049cd92e9eac9a322e3fe8ed61a2e5ac1ce5
nikita <n-tuganov@mail.ru>
2024-07-25 19:53:25 +0300

    page-list fix

--- src/main/kotlin/ru/tuganov/ShoppingTrackingAppApplication.kt
+++ src/main/kotlin/ru/tuganov/ShoppingTrackingAppApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TrackingAppApplication
-
-fun main(args: Array<String>) {
-    runApplication<TrackingAppApplication>(*args)
+class ShoppingTrackingAppApplication {
+    fun main(args: Array<String>) {
+        runApplication<ShoppingTrackingAppApplication>(*args)
+    }
 }
